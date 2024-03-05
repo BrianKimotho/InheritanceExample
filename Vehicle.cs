93fa1756b2f1b67b7134668a20a0c942ff0339e2
Brian Mwangi Kimotho <briankimotho@usf.edu>
2024-03-05 15:42:23 -0500

    Add project files.

---
+++ Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    class Vehicle
+    {
+        //Properties
+        public string Brand { get; set; }
+        public string Color { get; set; }
+
+        //Behaviors
+        public void
+    }
+}
